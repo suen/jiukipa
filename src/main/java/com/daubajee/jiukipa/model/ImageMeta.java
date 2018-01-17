b7f8b2686c6bf2809dbb100ab57e87f986fe8370
surendra <surendra.dangol@daubajee.com>
2018-01-17 15:50:25 +0100

    Add attr metadata, method tojson

--- src/main/java/com/daubajee/jiukipa/model/ImageMeta.java
+++ src/main/java/com/daubajee/jiukipa/model/ImageMeta.java
@@ -1,8 +1,11 @@
 package com.daubajee.jiukipa.model;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class ImageMeta {
@@ -15,16 +18,32 @@ public class ImageMeta {
 	
 	private final Date dateCreation;
 	
-    final SimpleDateFormat httpDateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private JsonObject metaJson;
 
-	public ImageMeta(String hash, int width, int height, Date dateCreation) {
+    public static final SimpleDateFormat EXIF_DATE_FORMAT = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+
+    public ImageMeta(String hash, int width, int height, Date dateCreation, JsonObject metaJson) {
 		this.hash = hash;
 		this.width = width;
 		this.height = height;
 		this.dateCreation = dateCreation;
+        this.metaJson = metaJson;
 	}
 
-	public String getHash() {
+    public ImageMeta(JsonObject metaInfMap) {
+        this.width = Integer.parseInt(metaInfMap.getString("tiff:ImageWidth"));
+        this.height = Integer.parseInt(metaInfMap.getString("tiff:ImageLength"));
+        String createDateStr = metaInfMap.getString("Date/Time");
+        try {
+            this.dateCreation = EXIF_DATE_FORMAT.parse(createDateStr);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+        this.hash = metaInfMap.getString("hashcode");
+        this.metaJson = metaInfMap;
+    }
+
+    public String getHash() {
 		return hash;
 	}
 
@@ -40,12 +59,26 @@ public class ImageMeta {
 		return dateCreation;
 	}
 
-	public JsonObject toJson() {
+    public JsonObject getMetaJson() {
+        return metaJson;
+    }
+
+    public void setMetaJson(JsonObject metaJson) {
+        this.metaJson = metaJson;
+    }
+
+    public List<String> getTags() {
+        JsonArray jsonArray = metaJson.getJsonArray("tags", new JsonArray());
+        return jsonArray.getList();
+    }
+
+    public JsonObject toJson() {
 		return new JsonObject()
 				.put("hash", hash)
 				.put("width", width)
 				.put("height", height)
-                .put("dateCreation", httpDateformat.format(dateCreation));
+                .put("tags", new JsonArray(getTags()))
+                .put("dateCreation", EXIF_DATE_FORMAT.format(dateCreation));
 	}
 	
 }
