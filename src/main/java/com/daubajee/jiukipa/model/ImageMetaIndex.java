476bb16a55b969fdd4cf50eb526e002b032cfea2
surendra <surendra.dangol@daubajee.com>
2018-01-17 15:51:31 +0100

    Add transformMetdata()

--- src/main/java/com/daubajee/jiukipa/model/ImageMetaIndex.java
+++ src/main/java/com/daubajee/jiukipa/model/ImageMetaIndex.java
@@ -1,23 +1,23 @@
 package com.daubajee.jiukipa.model;
 
-import java.text.SimpleDateFormat;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
+import com.beust.jcommander.internal.Lists;
+import com.daubajee.jiukipa.Event;
 import com.daubajee.jiukipa.EventTopics;
 import com.daubajee.jiukipa.batch.ImageSize;
+import com.google.common.collect.Sets;
 
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class ImageMetaIndex {
-
-    public static final SimpleDateFormat EXIF_DATE_FORMAT = new SimpleDateFormat(
-            "yyyy:MM:dd HH:mm:ss");
 
     private Map<String, ImageMeta> images = new HashMap<>();
 
@@ -40,6 +40,26 @@ public class ImageMetaIndex {
             eventBus.send(EventTopics.REQUEST_REPLAY_IMAGE_META,
                     "IMAGE_META_INDEX");
         }
+
+        eventBus.consumer(EventTopics.EVENT_STREAM, this::onEvent);
+
+    }
+
+    private void onEvent(Message<JsonObject> message) {
+        JsonObject json = message.body();
+        if (!json.getString("type", "").equals(Event.ADD_METADATA)) {
+            return;
+        }
+
+        Event event = Event.fromJson(json);
+        JsonObject eventData = event.getData();
+        String hash = eventData.getString("hash", "");
+        JsonObject metadata = eventData.getJsonObject("metaData", new JsonObject());
+        if (hash.isEmpty() || metadata.isEmpty()) {
+            return;
+        }
+
+        transformMetadata(hash, metadata);
     }
 
     private void onGetSize(Message<Object> message) {
@@ -61,14 +81,38 @@ public class ImageMetaIndex {
         return images.values();
     }
 
+    public void transformMetadata(String hash, JsonObject metadata) {
+        JsonObject copy = metadata.copy();
+        copy.remove("hash");
+        copy.remove("width");
+        copy.remove("height");
+        ImageMeta imageMeta = images.get(hash);
+        if (imageMeta == null) {
+            return;
+        }
+
+        JsonObject metaJson = imageMeta.getMetaJson();
+        Map<String, Object> map = metadata.getMap();
+        map.entrySet().forEach(kv -> {
+            Object value = kv.getValue();
+            String key = kv.getKey();
+            if (value instanceof JsonArray) {
+                JsonArray existing = metaJson.getJsonArray(key, new JsonArray());
+                Set newTagSet = Sets.newHashSet(existing.getList());
+                newTagSet.addAll(((JsonArray) value).getList());
+                metaJson.put(key, new JsonArray(Lists.newArrayList(newTagSet)));
+            } else if (value instanceof JsonObject) {
+                System.out.println("No implement !!");
+            } else {
+                metaJson.put(key, value);
+            }
+        });
+
+    }
+
     private static ImageMeta createImageMeta(JsonObject metaInfMap) {
         try {
-            Integer width = Integer.parseInt(metaInfMap.getString("tiff:ImageWidth"));
-            Integer height = Integer.parseInt(metaInfMap.getString("tiff:ImageLength"));
-            String createDateStr = metaInfMap.getString("Date/Time");
-            Date creationDate = EXIF_DATE_FORMAT.parse(createDateStr);
-            String hash = metaInfMap.getString("hashcode");
-            ImageMeta image = new ImageMeta(hash, width, height, creationDate);
+            ImageMeta image = new ImageMeta(metaInfMap);
             return image;
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
